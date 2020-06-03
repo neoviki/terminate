30cc7c679c3ef6743d06e0b15d5c1f7870b5c4c4
Vignesh Natarajan <vikiworks.io@gmail.com>
2020-06-02 21:28:59 -0400

    unit test case

---
+++ unit_test/test_terminate_force.sh
@@ -0,0 +1,38 @@
+echo
+echo "[                 Unit Test              ]"
+echo
+echo "[ status ] simulating dummy process 1"
+/bin/bash ./dummy_process.sh &
+sleep 1
+echo "[ status ] simulating dummy process 2"
+/bin/bash ./dummy_process.sh &
+sleep 1
+echo "[ status ] simulating dummy process 3"
+/bin/bash ./dummy_process.sh &
+sleep 1
+echo "[ status ] simulating dummy process 4"
+/bin/bash ./dummy_process.sh &
+sleep 1
+echo "[ status ] simulating dummy process 5"
+/bin/bash ./dummy_process.sh &
+sleep 1
+echo "[ status ] simulating dummy process 6"
+/bin/bash ./dummy_process.sh &
+sleep 1
+echo
+echo
+echo "[ status ] terminating dummy process"
+../src/terminate "dummy_process.sh" -force
+echo
+echo
+
+sleep 6
+
+RES=`ps -x | grep "dummy_process.sh" | grep -v grep`
+
+if [ -z $RES ]; then
+    echo "[ status ] terminate force is working fine"
+else
+    echo "[ error ] terminate force failed"
+fi
+
